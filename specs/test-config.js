15d43444659ad22eae6224752b6be4f41d82fbe6
Christian Hubinger <christian@trigo.at>
2018-01-25 15:46:44 +0100

    fix: tests runs & config mgmt

--- specs/test-config.js
+++ specs/test-config.js
@@ -1,18 +1,8 @@
 'use strict';
 
 module.exports = {
-	keycloakApi: {
-		authorizationEndpoint: process.env.KEYCLOAK_URL || 'http://localhost:8080/auth',
-		clientId: 'admin-cli',
-		username: 'admin',
-		password: 'password',
-	},
-	serviceAccount: {
-		clientId: 'provisioner-service',
-		clientSecret: '00000000-0000-0000-0000-000000000042',
-	},
-	realm: 'oegb-verlag-test',
-	clientId: 'erecht-web-test',
-	clientSecret: 'secret',
-	resetTokenTTL: 300, // 5 Minutes
+	authorizationEndpoint: process.env.KEYCLOAK_URL || 'http://localhost:8080/auth',
+	// clientId: 'admin-cli',
+	// username: 'admin',
+	// password: 'password',
 };
